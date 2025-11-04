0df81d59adebd7c5e3be4bb47af467d808e371d2
João Pedro <Joaoargento12@gmail.com>
2025-11-04 15:42:05 -0300

    feat(backendSetup): backend setup and dependencies installation

---
+++ back/src/Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+namespace Hypesoft.Domain;
+
+public class Product
+{
+    public enum ProductCategory
+    {
+        A,
+        B,
+        C
+    }
+    private string name;
+    private string description;
+    private decimal price;
+    private int amountInStock;
+    private ProductCategory category;
+
+    public Product(string name, string description, decimal price, ProductCategory category, int amountInStock)
+    {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.amountInStock = amountInStock;
+    }
+
+    public void UpdatePrice(decimal newPrice)
+    {
+        this.price = newPrice;
+    }
+} 
